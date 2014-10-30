fb5d8f55c7df75bc86b32cffe638598966696870
jfarid27 <jrab227@gmail.com>
2014-10-30 18:52:44 -0400

    Added restful available datasets and analysis types. fixes #6. fixes #7

--- src/Panda/PandaRest.js
+++ src/Panda/PandaRest.js
@@ -30,6 +30,42 @@ function getFile(request, response){
 
 }
 
+function getTypes(request, response){
+
+  console.log("Received get types request.\n")
+
+
+  if (!request.params) {
+    console.log("Received bad request!")
+    response.status(501).send({error: "No request params set!"})
+    return
+  }
+
+  var inst = pandaController.connectToEmitter()
+  var resWrapper = function(data) {
+    response.json(data)
+  }
+  var resErrWrapper = function(msg) {
+    response.status(500).send({error:msg})
+  }
+  
+  if (request.param('folder') ) {
+
+    console.log('Available datasets request made')
+    //unwrap request
+    var params = {folder: request.param('folder')}
+    inst.emit('listAvailableFilesEvent', params, resWrapper, resErrWrapper)
+    return
+  } else if (!request.param('folder')) {
+    inst.emit('availableFoldersEvent', params, resWrapper, resErrWrapper)
+    return
+  } else {
+    response.status(500).send({error:"Couldn't parse request"})
+    return
+  }
+
+
+}
 
 
 exports.rest = function(app) {
@@ -37,6 +73,7 @@ exports.rest = function(app) {
   app.route('/graph')
     .get(getFile)
 
-  
+  app.route('/types')
+    .get(getTypes)
 
 }
