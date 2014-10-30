4cd49741942774b95f88fa7546ffeaeddb3cc010
jfarid27 <jrab227@gmail.com>
2014-10-30 18:36:42 -0400

    Updates for relative filepath.

--- src/Panda/PandaController.js
+++ src/Panda/PandaController.js
@@ -5,7 +5,8 @@ var events = require('events'),
   fs = require('fs'),
   arraysToGraphEdges = require('./arrayToGraphEdge.js').arraysToGraphEdgesSync
 
-var pathToFiles = './data';
+//Relative to server start for fs module
+var pathToFiles = './src/Panda/data';
 
 
 var PandaController = function(){
@@ -17,11 +18,16 @@ var PandaController = function(){
 
     var filePath = pathToFiles + '/' + params.folder + '/' + params.file
     var fileConnection = fs.createReadStream(filePath)
-    
+     
     var connection = readableStreamEmitter
       .connectToStream(fileConnection, arraysToGraphEdgesWrapper(callback))
     
-    connection.on('error', function(){ err() })
+    connection.on('error', function(error){ 
+
+      var message = "ReadableStreamEmitter Error"
+      err(message) 
+
+    })
 
   })
 
