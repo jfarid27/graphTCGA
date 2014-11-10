299c221722aa32cda419c3c002fd72da49eb40da
jfarid27 <jrab227@gmail.com>
2014-11-10 18:11:23 -0500

    added minimist. fixes #26. fixes #27.

--- index.js
+++ index.js
@@ -1,3 +1,22 @@
-var server = require('./server.js')
+var argvs = require('minimist')(process.argv.slice(2))
 
-server.start()
+if (!argvs) {
+    console.error("Error: No input args")
+}
+
+if (argvs['p'] && !argvs['t'] && !argvs['i'] && !argvs['output']) {
+    var server = require('./server.js')
+    server.start(argvs['p'])
+}
+
+if (argvs['z'] && argvs['i'] && argvs['_'].length == 1 && argvs['output']){
+    //print out to output file
+
+
+}
+
+if (argvs['z'] && argvs['i'] && argvs['_'].length == 1){
+    //Dump to stdout
+
+}
+
