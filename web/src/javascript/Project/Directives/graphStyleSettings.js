45905c158499ac8440b7c7378489ffb6b537ba0d
jfarid27 <jrab227@gmail.com>
2014-12-10 17:31:39 -0500

    updated graph style

--- web/src/javascript/Project/Directives/graphStyleSettings.js
+++ web/src/javascript/Project/Directives/graphStyleSettings.js
@@ -6,24 +6,28 @@
             {
                 selector: 'node',
                 css: {
-                    'background-color': 'red',
+                    'background-color': '#001fa2',
                     'content': 'data(id)',
-                    'color': 'black'
+                    'color': 'black',
+                    'width':45,
+                    'height':45,
+                    'font-size': 57
                 }
             },
             {
                 selector: 'edge',
                 css: {
                     'target-arrow-shape': 'triangle',
-                    'line-color': 'grey'
+                    'line-color': '#000000',
+                    'target-arrow-fill':'filled',
+                    'width':13,
+                    'target-arrow-color': '#000',
                 }
             },
             {
-                selector: 'edge-arrow',
+                selector: 'graph-background',
                 css: {
-                    'target-arrow-shape': 'triangle',
-                    'target-arrow-color': 'grey',
-                    'line-color': 'grey'
+                    'overlay-color': 'green'
                 }
             }
         ]
