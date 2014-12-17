d28fea0efd6abc94217dadc5c8f487554a9d27bd
jfarid27 <jrab227@gmail.com>
2014-12-17 13:29:38 -0500

    added starting search library

---
+++ web/src/javascript/Project/Factories/SearchCorpus.js
@@ -0,0 +1,17 @@
+(function(){
+    define([], function(){
+        return [
+            {'name':'Breast invasive carcinoma', 'value':'BRCA'},
+            {'name':'BRCA', 'value':'BRCA'},
+            {'name':'Colon adenocarcinoma', 'value':'COAD'},
+            {'name':'COAD', 'value':'COAD'},
+            {'name':'Glioblastoma multiforme', 'value':'GBM'},
+            {'name':'GBM', 'value':'GBM'},
+            {'name':'Kidney renal clear cell carcinoma', 'value':'KIRC'},
+            {'name':'KIRC', 'value':'KIRC'},
+            {'name':'Kidney renal papilary cell carcinoma', 'value':'KIRP'},
+            {'name':'KIRP', 'value':'KIRP'},
+        ]
+
+    })
+})()
